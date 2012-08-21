c49b2aba4b8dba88bf9c9064daf8c07075b60c4a
Robin Lambertz <robinlambertz@gmail.com>
2012-08-21 22:15:19 +0100

    Fixed pom.xml

--- src/com/craftminecraft/craftchat/listeners/ChatListener.java
+++ src/com/craftminecraft/craftchat/listeners/ChatListener.java
@@ -7,17 +7,14 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.Bukkit;
 
-import net.milkbowl.vault.chat.Chat;
+import com.craftminecraft.craftchat.CraftChat;
 
 public class ChatListener implements Listener {
 
-    private FileConfiguration config;
-    private Chat chat;
-    public ChatListener(FileConfiguration config){
-        this.config = config;
-        RegisteredServiceProvider<Chat> rsp = Bukkit.getServer().getServicesManager().getRegistration(Chat.class);
-        this.chat = rsp.getProvider();
+    private CraftChat plugin;
 
+    public ChatListener(CraftChat plugin){
+        this.plugin = plugin;
     }
 
     @EventHandler
@@ -25,10 +22,10 @@ public class ChatListener implements Listener {
         if(event.isCancelled()){
             return;
         } else {
-            String format = this.config.getString("craftchat.format");
+            String format = this.plugin.getConfig().getString("craftchat.format");
             format.replace("{WORLDNAME}", event.getPlayer().getWorld().getName());
-            format.replace("{PREFIX}", chat.getPlayerPrefix(event.getPlayer()));
-            format.replace("{SUFFIX}", chat.getPlayerSuffix(event.getPlayer()));
+            format.replace("{PREFIX}", CraftChat.chat.getPlayerPrefix(event.getPlayer()));
+            format.replace("{SUFFIX}", CraftChat.chat.getPlayerSuffix(event.getPlayer()));
             format.replace("{REALNAME}", event.getPlayer().getName());
             format.replace("{DISPLAYNAME}", event.getPlayer().getDisplayName());
             format.replace("{CHANNEL}", "G");
